2dcef7b2ff78a789e37a30a58afbb373184d0b74
muzman01 <supurge_man@hotmail.com>
2024-11-10 18:00:13 +0300

    asdasdas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { fetchUser } from "./store/thunks/fetchUser";
 import { AppDispatch } from "./store";
 
@@ -13,53 +13,30 @@ import WaitingRoom from "./pages/WaitingRoom";
 import GameRoom from "./pages/GameRoom";
 import Tasks from "./pages/Tasks";
 import Market from "./pages/Market";
-import { useTelegram } from "./context/TelegramContext";
+import WaitingRoomBot from "./pages/WaitingRoomBot";
+import GameRoomBots from "./pages/GameRoomBots";
 
 function App() {
-  // const dispatch = useDispatch<AppDispatch>(); // dispatch'i AppDispatch olarak tipliyoruz
+  const dispatch = useDispatch<AppDispatch>(); // dispatch'i AppDispatch olarak tipliyoruz
 
-  // // staticUser değişkeni
-  // const staticUser = {
-  //   telegram_id: 313131, // Zorunlu alan
-  //   first_name: "John", // İsteğe bağlı
-  //   last_name: "Doe", // İsteğe bağlı
-  //   username: "johndoe", // İsteğe bağlı
-  //   photo_url: "https://example.com/photo.jpg", // İsteğe bağlı
-  //   language_code: "en", // İsteğe bağlı
-  // };
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const result = await dispatch(fetchUser(staticUser)); // async dispatch
-  //     console.log(result);
-  //   };
-
-  //   fetchData(); // Fetch user'ı çağır
-  // }, [dispatch]);
-
-
-
- const dispatch = useDispatch<AppDispatch>();
-  const { telegramUser } = useTelegram();
-  const [isLoading, setIsLoading] = useState(true);
+  // staticUser değişkeni
+  const staticUser = {
+    telegram_id: 313131, // Zorunlu alan
+    first_name: "John", // İsteğe bağlı
+    last_name: "Doe", // İsteğe bağlı
+    username: "johndoe", // İsteğe bağlı
+    photo_url: "https://example.com/photo.jpg", // İsteğe bağlı
+    language_code: "en", // İsteğe bağlı
+  };
 
   useEffect(() => {
     const fetchData = async () => {
-      if (telegramUser) {
-        setIsLoading(true); // Başlangıçta yükleme durumunu true yap
-        const result = await dispatch(fetchUser(telegramUser));
-        console.log(result);
-        setIsLoading(false); // Veriler yüklendikten sonra false yap
-      }
+      const result = await dispatch(fetchUser(staticUser)); // async dispatch
+      console.log(result);
     };
 
-    fetchData();
-  }, [dispatch, telegramUser]);
-
-  if (isLoading || !telegramUser) {
-    return <div>Loading user data...</div>; // Yükleyici göster
-  }
-
+    fetchData(); // Fetch user'ı çağır
+  }, [dispatch]);
 
   return (
     <Router>
@@ -82,8 +59,16 @@ function App() {
           element={<WaitingRoom />}
         />
         <Route
+          path="/salon/:salon_id/table/:table_id/waiting-room-bot"
+          element={<WaitingRoomBot />}
+        />
+        <Route
           path="/game/:salon_id/:table_id/game-room"
           element={<GameRoom />}
+        />
+        <Route
+          path="/game/:salon_id/:table_id/game-room-bots"
+          element={<GameRoomBots />}
         />
       </Routes>
     </Router>
